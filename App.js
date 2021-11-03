06767bde9864efb92753aff9bedc9510270c52f8
mitshivam10 <mitshivam10@users.noreply.github.com>
2021-11-03 03:00:53 -0700

    Added common style sheet for consistency
    Please use CommonStyleSheet.js for styling to keep consistency all over app and reusability.

--- App.js
+++ App.js
@@ -6,26 +6,30 @@ import { Marker } from "react-native-maps";
 // import gpxParser from "gpxparser";
 import MapComponent from "./MapComponent";
 
+import CommonStyleSheet from "./CommonStyleSheet";
+
 const windowHeight = Dimensions.get("window").height;
 
 export default function App() {
-	// var track = parseGpx(gpxContent);
-	return (
-		<View>
-			<MapComponent />
-		</View>
-	);
+  // var track = parseGpx(gpxContent);
+  return (
+    <View>
+      <MapComponent />
+
+      {/* <Text style={CommonStyleSheet.map}></Text> */}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	map: {
-		...StyleSheet.absoluteFillObject,
-		height: windowHeight
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    height: windowHeight,
+  },
 });
---
+++ CommonStyleSheet.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+// An CommonStyleSheet used for the purpose of consistency and reuse the same style
+// in different Components.
+export default CommonStyleSheet = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    height: windowHeight,
+  },
+});
