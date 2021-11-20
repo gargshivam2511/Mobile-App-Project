bd9b0a42f515ad3ecbda27e148959a37a5e479ce
Liam Scholte <scholte.l@northeastern.edu>
2021-11-19 18:50:45 -0800

    Improves track styling

--- TrackComponent.js
+++ TrackComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { View } from "react-native";
 import { Polyline } from "react-native-maps";
 
 export default class TrackComponent extends Component {
@@ -8,22 +9,29 @@ export default class TrackComponent extends Component {
 	}
 
 	render() {
+		let coordinates = this.props.track.segments[0].points.map(function (point) {
+			return {
+				latitude: point.lat,
+				longitude: point.lon
+			};
+		});
 		return (
-			<Polyline
-				coordinates={this.props.track.segments[0].points.map(function (point) {
-					return {
-						latitude: point.lat,
-						longitude: point.lon
-					};
-				})}
-				lineDashPattern={[1]}
-				strokeColor={this.props.color}
-				strokeWidth={4}
-				tappable={true}
-				onPress={() => {
-					this.props.onPress();
-				}}
-			/>
+			<View>
+				<Polyline
+					coordinates={coordinates}
+					strokeColor="white"
+					strokeWidth={8}
+					tappable={true}
+					onPress={() => {
+						this.props.onPress();
+					}}
+				/>
+				<Polyline
+					coordinates={coordinates}
+					strokeColor={this.props.color}
+					strokeWidth={4}
+				/>
+			</View>
 		);
 	}
 }
