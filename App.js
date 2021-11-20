51593f1a1faa9098cf4b5849cc61380b966aeff9
Liam Scholte <scholte.l@northeastern.edu>
2021-11-19 23:17:16 -0800

    Cleans up App.js

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import MapView from "react-native-maps";
-import { Marker } from "react-native-maps";
-// import gpxParser from "gpxparser";
+import { View } from "react-native";
 import MapComponent from "./MapComponent";
 
-import CommonStyleSheet from "./CommonStyleSheet";
-
-const windowHeight = Dimensions.get("window").height;
-
 export default function App() {
-  // var track = parseGpx(gpxContent);
-  return (
-    <View>
-      <MapComponent />
-
-      {/* <Text style={CommonStyleSheet.map}></Text> */}
-    </View>
-  );
+	return (
+		<View>
+			<MapComponent />
+		</View>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-    height: windowHeight,
-  },
-});
