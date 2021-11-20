807b703bb2308188cd7b280840aaff56a483f516
Liam Scholte <scholte.l@northeastern.edu>
2021-11-20 00:04:15 -0800

    Creates a ToolbarButton component consisting of text and an SVG

---
+++ components/ToolbarButton.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Text, TouchableHighlight, View } from "react-native";
+import CommonStyleSheet from "./CommonStyleSheet";
+
+export default class ToolbarButton extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<TouchableHighlight
+				style={CommonStyleSheet.button}
+				onPress={this.props.onPress}
+			>
+				<View>
+					{
+						<this.props.svg
+							style={CommonStyleSheet.svgButton}
+							width={25}
+							height={25}
+						/>
+					}
+					<Text style={CommonStyleSheet.buttonText}>{this.props.text}</Text>
+				</View>
+			</TouchableHighlight>
+		);
+	}
+}
